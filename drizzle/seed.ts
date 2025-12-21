3ecac4dfe1387866f3546408d8310b68c8a6fbb5
julianostroschon <julianostroschon@gmail.com>
2025-12-21 18:33:43 -0300

    raw(deps): altera bcrypt para bcryptjs

--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -1,4 +1,4 @@
-import { hash } from "bcrypt";
+import { hash } from "bcryptjs";
 import { db } from "./db";
 import { categories, transactions, users, usersWallets, wallets } from "./schema";
 
--- src/routes/userAuth.ts
+++ src/routes/userAuth.ts
@@ -1,4 +1,4 @@
-import { compare, hash } from "bcrypt";
+import { compare, hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { FastifyInstance } from "fastify";
 import jwt from "jsonwebtoken";
